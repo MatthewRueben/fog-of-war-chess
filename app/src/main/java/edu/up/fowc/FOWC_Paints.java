74deab48e23ca47c799497ceef2b9b106915479a
Matthew Rueben <rueben@up.edu>
2026-05-22 17:27:24 -0700

    Draw board.

---
+++ app/src/main/java/edu/up/fowc/FOWC_Paints.java
@@ -0,0 +1,43 @@
+package edu.up.fowc;
+
+import android.graphics.Color;
+import android.graphics.Paint;
+
+public class FOWC_Paints {
+
+    Paint background = new Paint();
+    Paint boardLine = new Paint();
+    Paint whiteSquare = new Paint();
+    Paint whitePieceFill = new Paint();
+    Paint whitePieceStroke = new Paint();
+    Paint blackSquare = new Paint();
+    Paint blackPieceFill = new Paint();
+    Paint blackPieceStroke = new Paint();
+
+    public FOWC_Paints()
+    {
+        this.background.setColor(Color.GRAY); // Style is unused.
+
+        this.boardLine.setColor(Color.DKGRAY);
+        this.boardLine.setStyle(Paint.Style.STROKE);
+
+        this.whiteSquare.setColor(Color.WHITE);
+        this.whiteSquare.setStyle(Paint.Style.FILL);
+
+        this.whitePieceFill.setColor(Color.WHITE);
+        this.whitePieceFill.setStyle(Paint.Style.FILL);
+
+        this.whitePieceStroke.setColor(Color.BLACK);
+        this.whitePieceStroke.setStyle(Paint.Style.STROKE);
+
+        this.blackSquare.setColor(Color.BLACK);
+        this.blackSquare.setStyle(Paint.Style.FILL);
+
+        this.blackPieceFill.setColor(Color.BLACK);
+        this.blackPieceFill.setStyle(Paint.Style.FILL);
+
+        this.blackPieceStroke.setColor(Color.WHITE);
+        this.blackPieceStroke.setStyle(Paint.Style.STROKE);
+    }
+
+}
--- app/src/main/java/edu/up/fowc/FOWC_View.java
+++ app/src/main/java/edu/up/fowc/FOWC_View.java
@@ -1,18 +1,58 @@
 package edu.up.fowc;
 
 import android.content.Context;
-import android.graphics.Color;
+import android.graphics.Canvas;
 import android.graphics.Paint;
 import android.util.AttributeSet;
 import android.view.SurfaceView;
 
 
 public class FOWC_View extends SurfaceView {
+
+    private FOWC_State fowcState;
+
+    private FOWC_Paints paints;
+
+    // TODO: change to proportions of view size.
+    // TODO: wrap in separate class.
+    public static final float boardTop = 100.0f;
+    public static final float boardLeft = 100.0f;
+    public static final float squareSideLength = 100.0f;
+
+
     public FOWC_View(Context context, AttributeSet attrs) {
         super(context, attrs);
 
-        setWillNotDraw(false);
+        this.fowcState = new FOWC_State();
 
-        setBackgroundColor(Color.WHITE);  //better than black default
+        this.paints = new FOWC_Paints();
+        setBackgroundColor(this.paints.background.getColor());
+        setWillNotDraw(false);
     }
+
+    @Override
+    public void onDraw(Canvas canvas)
+    {
+        for (int rowShift = 0; rowShift < FOWC_State.NUM_RANKS; rowShift++)
+        {
+            for (int colShift = 0; colShift < FOWC_State.NUM_FILES; colShift++)
+            {
+                float squareLeft = boardLeft + colShift * squareSideLength;
+                float squareTop = boardTop + rowShift * squareSideLength;
+                float squareRight = squareLeft + squareSideLength;
+                float squareBottom = squareTop + squareSideLength;
+                boolean squareIsWhite = (rowShift + colShift) % 2 == 0; // This implements the standard "white is on the (bottom) right" layout.
+                Paint squarePaint = squareIsWhite ? this.paints.whiteSquare : this.paints.blackSquare;
+                canvas.drawRect(squareLeft, squareTop, squareRight, squareBottom, squarePaint);
+            }
+        }
+
+
+
+        //canvas.drawCircle(500.0f, 500.0f, 100.0f, this.paints.whitePieceFill);
+        //canvas.drawCircle(500.0f, 500.0f, 100.0f, this.paints.whitePieceStroke);
+
+    }
+
+
 }
